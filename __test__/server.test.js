bd0a8969c6e74c3880c9b28eb3084c430c669ef2
Malek Al-Sheekh Hasan <malek.alsheekh01@gmail.com>
2023-06-19 19:21:38 +0000

    class 06 is done

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,49 +1,49 @@
-// "use strict"
-// const {db}=require("../src/auth/models/index")
-// const {app}=require("../src/server")
-// const supertest=require("supertest")
-// const request=supertest(app)
+"use strict"
+const {db}=require("../src/auth/models/index")
+const {app}=require("../src/server")
+const supertest=require("supertest")
+const request=supertest(app)
 
-// beforeAll(async () => {
-//     await db.sync();
-// });
-// describe("Testing Server signup endpoint",()=>{
-//     test("Test Wrong path ",async ()=>{
-//        const result=await request.get("/sign");
-//        expect(result.status).toBe(404)
-//     })
-//     test("Test signup ",async ()=>{
-//         const result=await request.post("/signup").send({
-//             username: 'malek',
-//             password: '1234'
-//         });
-//         expect(result.status).toBe(201);
-//      });
-//      test("Test signup with duplicate data",async ()=>{
-//         const result=(await request.post("/signup")).send({
-//             username: 'malek',
-//             password: '1233'
-//         });
-//         expect(result.status).toBe(201)})
- 
+beforeAll(async () => {
+    await db.sync();
+});
 
-// })
-// describe("Testing Server signin endpoint",()=>{
-//     test("Test Wrong path ",async ()=>{
-//        const result=await request.get("/sign");
-//        expect(result.status).toBe(404)
-//     })
-//     test("Test signin ",async()=>{
-//         const result=await request.post("/signin").send()
-//         expect(result.status).toBe(201);
-//      });
-//      test("Test signin with wrong data",async ()=>{
-//         const result=(await request.post("/signin"))
-//         expect(result.status).toBe(200)
-//      })
-// })
+describe("Testing Server signup endpoint",()=>{
+    test("Test Wrong path ",async ()=>{
+       const result=await request.get("/sign");
+       expect(result.status).toBe(404)
+    })
+    test("Test signup ",async ()=>{
+        const result=await request.post("/signup").send({
+            username: 'malek',
+            password: '123'
+        });
+        expect(result.status).toBe(201);
+     }); 
+})
+describe("Testing Server signin endpoint",()=>{
+    // let req={
+    //     headers: {
+    //     authorization: 'Basic bWFsZWs6MTIz', // base64 encoded "username:password"
+    //   },
+    //   body: {},
+    // }
+    test("Test Wrong path ",async ()=>{
+       const result=await request.post("/sign");
+       expect(result.status).toBe(404)
+    })
+        // test("Test sign in",async ()=>{
+        //     const result =await request.post("/signin").send({
+        //         username:"malek",
+        //         password:"123"
+        //     })
+        //     expect(result.status).toBe(200)
+
+        // })
+
+})
 
 
-// afterAll(async () => {
-//     await db.drop();
-// });
+afterAll(async () => {
+    await db.drop();
+});
